8b93969b5d65a09589aebfbcb4f2d569ff5f86c4
Gideon <kipkerring.gedion@gmail.com>
2023-10-26 12:18:01 +0300

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,16 +18,16 @@ def validUTF8(data):
         binary = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
         if count == 0:
             if binary.startswith('110'):
-                count == 1
+                count = 1
             if binary.startswith('1110'):
-                count == 2
+                count = 2
             if binary.startswith('11110'):
-                count == 3
+                count = 3
             if binary.startswith('10'):
                 return False
         else:
             if not binary.startswith('10'):
-                return True
+                return False
             count -= 1
 
     if count != 0:
