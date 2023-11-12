f63bf45922bccc636f9d903a34e115fe528b4669
Gideon <kipkerring.gedion@gmail.com>
2023-11-12 12:57:57 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const request = require('request')
-const movieId = process.argv[2]
+const request = require('request');
+const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, async(err, response, body) => {
+request(url, async (err, response, body) => {
   if (err) {
     console.log(err);
   }
